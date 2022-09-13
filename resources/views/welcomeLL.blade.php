af97cfe5c155d8beeae42fe14e41d595a934ce53
Alanbp98 <alanbp98@gmail.com>
2022-09-13 08:44:48 +0700

    membuat halaman dasar project dengan laravel

---
+++ resources/views/welcomeLL.blade.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Legal Literacy</title>
+</head>
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container d-flex justify-content-between">
+            <div class="logo">
+                <a href="welcomeLL.blade.php">
+                    <h1><img src="logo.png" alt="Legal Literacy" width="60"> Legal Literacy</h1></a>
+            </div>
+        
+        <!-- navbar -->
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li>
+                    <a class="nav-link scroll active" href="#kategori">Kategori</a>
+                </li>
+                <li>
+                    <a class="nav-link scroll active" href="#about">Tentang kami</a>
+                </li>
+                <li>
+                    <a class="nav-link scroll active" href="#testimoni">Testimoni</a>
+                </li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+
+    <!-- ======= Home ======= -->
+    <section id="Home" class="clearfix">
+        <div class="container aos-init-animate" data-aos="fade-up">
+            <div class="home-image aos-init aos-animate" data-aos="zoom-out" data-aos-delay="200">
+                <img src="home.png" alt class="img-fluid">
+            </div>
+            <div class="home-info aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+                <h2>
+                    "We provide"
+                    <br>
+                    <span><u>solutions</u></span>
+                    <br>
+                    "For your legal problems!"
+                </h2>   
+                <div>
+                    <a href="#gaskeun" class="btn-get-started scrollto">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ======= Main ======= -->
+    <main id="main">
+        <!-- ======= Kategori ======= -->
+        <section id="kategori">
+            <div class="container aos-init aos-animate" data-aos="fade-up">
+                <header class="section-header">
+                    <h3>Kategori</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cupiditate quasi incidunt error eos ullam unde modi sequi excepturi temporibus odio accusamus expedita ratione nulla cum nihil nisi, ad assumenda.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nam aliquam velit quia iste eius expedita possimus corrupti, tempora deleniti, reprehenderit esse aperiam similique doloribus officiis libero, non quos beatae?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At dignissimos esse laudantium necessitatibus laboriosam, minus accusantium sapiente eaque sed nihil odit, eos, repellat eveniet consequuntur neque magni soluta exercitationem. Ab.
+                    </p>
+                    <br><br><br><br><br><br><br><br><br><br><br>
+                </header>
+            </div>
+        </section>
+
+        <!-- ======= About ======= -->
+        <section id="about">
+            <div class="container aos-init aos-animate" data-aos="fade-up">
+                <header class="section-header">
+                    <h3>Tentang Kami</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cupiditate quasi incidunt error eos ullam unde modi sequi excepturi temporibus odio accusamus expedita ratione nulla cum nihil nisi, ad assumenda.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nam aliquam velit quia iste eius expedita possimus corrupti, tempora deleniti, reprehenderit esse aperiam similique doloribus officiis libero, non quos beatae?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At dignissimos esse laudantium necessitatibus laboriosam, minus accusantium sapiente eaque sed nihil odit, eos, repellat eveniet consequuntur neque magni soluta exercitationem. Ab.
+                    </p>
+                    <br><br><br><br><br><br><br><br><br><br><br>
+                </header>
+            </div>
+        </section>
+
+        <!-- ======= Testimoni ======= -->
+        <section id="testimoni">
+            <div class="container aos-init aos-animate" data-aos="fade-up">
+                <header class="section-header">
+                    <h3>Testimoni</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cupiditate quasi incidunt error eos ullam unde modi sequi excepturi temporibus odio accusamus expedita ratione nulla cum nihil nisi, ad assumenda.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil nam aliquam velit quia iste eius expedita possimus corrupti, tempora deleniti, reprehenderit esse aperiam similique doloribus officiis libero, non quos beatae?
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At dignissimos esse laudantium necessitatibus laboriosam, minus accusantium sapiente eaque sed nihil odit, eos, repellat eveniet consequuntur neque magni soluta exercitationem. Ab.
+                    </p>
+                    <br><br><br><br><br><br><br><br><br><br><br>
+                </header>
+            </div>
+        </section>
+
+        <!-- ======= Get Started ======= -->
+        <section id="gaskeun">
+            <div class="container aos-init aos-animate" data-aos="fade-up">
+                <header class="section-header">
+                    <h3>Get Started</h3>
+                    <a><img src="registrasi.png" alt="registrasi" width="25%"></a>
+                    <a><img src="login.png" alt="login" width="39%"></a>
+                </header>
+            </div>
+        </section>
+    </main>
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li>
+                                <a href="#">Beranda</a>
+                            </li> 
+                            <li>
+                                <a href="#">Kategori</a>
+                            </li>    
+                            <li>
+                                <a href="#">Tentang Kami</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimoni</a>
+                            </li> 
+                            <li>
+                                <a href="#">Get Started</a>
+                            </li>  
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 footer-contact">
+                        <h4>Contact Us</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam amet natus voluptate corporis, impedit expedita vero harum suscipit voluptatem quis repellendus nisi alias? Accusantium odio sit mollitia sapiente totam culpa.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sapiente maiores et provident maxime aut voluptatibus. Voluptates ut, itaque quaerat modi est consectetur aspernatur nostrum, delectus, odit nemo neque? Ducimus!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus aut repellendus, amet minima tempore adipisci aliquam maxime temporibus quos id ab soluta, iusto aperiam vel neque quod tenetur deserunt veniam?
+                        </p>
+                        <div class="social-links">
+                            <a href="#" class="twitter">
+                                <i class="bi bi-twitter">
+                                    ::before
+                                </i>
+                            </a>
+                            <a href="#" class="facebook">
+                                <i class="bi bi-facebook">
+                                    ::before
+                                </i>
+                            </a>
+                            <a href="#" class="instagram">
+                                <i class="bi bi-instagram">
+                                    ::before
+                                </i>
+                            </a>
+                            <a href="#" class="linkedin">
+                                <i class="bi bi-linkedin">
+                                    ::before
+                                </i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 footer-newsletter">
+                        <h4>Our Newsletter</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officiis iste sunt? Minima excepturi cupiditate, tempore eveniet sit aliquid saepe, ipsum modi consequatur eaque rem eos animi veniam qui perferendis!
+                        </p>
+                        <form action method="post">
+                            <input type="email" name="email">
+                            <input type="submit" value="Subscribe">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                " @ copyright "
+                <strong>Legal Literacy</strong>
+                ". All Right Reversed "
+            </div>
+            <div class="credits">
+                <!-- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam laboriosam ipsum eos aut est similique fuga aliquid fugiat quas, obcaecati eum rem, cum voluptates debitis alias veniam? Ad, culpa possimus? -->
+                " Designed by"
+                ".........."
+                {{-- <a href="https://bootstrapmade.com/">BootstrapMade</a> --}}
+            </div>
+        </div>
+    </footer>
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center active"><i class="bi bi-arrow-up-short"></i>
+        <i class="bi bi-arrow-up-short">
+        ::before
+        </i>
+    </a>
+
+</body>
+</html>
